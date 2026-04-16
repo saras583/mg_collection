10908efc61240b3316a8573481dd7f79a7cb666f
saras583 <sarasskumar2004@gmail.com>
2026-04-16 21:19:56 +0530

    upate

--- lib/screens/cart.dart
+++ lib/screens/cart.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
 class Cart extends StatefulWidget {
   const Cart({super.key});
@@ -11,26 +12,38 @@ class _CartState extends State<Cart> {
   List<int> quantity = [1, 1, 1];
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Color(0xFFF5EFEF),
-    body: SafeArea(child: Column(
-      children: [
-        Padding(padding: EdgeInsets.all(10),
-        child: Row(children: [
-          CircleAvatar(backgroundColor: Colors.white,
-          child: Icon(Icons.arrow_back)),
-          SizedBox(width: 100,)
-          ,Text('My Cart',style: TextStyle(fontSize: 18,
-                      fontWeight: FontWeight.bold),),
+    var box = Hive.box('cart');
 
-        ],),),
-        //cartitems
-        Expanded(child: ListView(
-          children: [
-            
-          ],
-        ))
-      ],
-    )),);
+    return Scaffold( appBar: AppBar( title: Text('MyCart',),),
+      backgroundColor: Color(0xFFF5EFEF),
+      body: SafeArea(
+        child: ValueListenableBuilder(
+          valueListenable: box.listenable(),
+          builder: (context, Box box, _) {
+            if (box.isEmpty) {
+              return Center(child: Text('Cart is emty'));
+            }
+            return ListView.builder(
+              itemCount: box.length,
+              itemBuilder: (BuildContext context, int index) {
+                final items = box.getAt(index);
+                if (items == null) return SizedBox();
+                return ListTile(
+                  leading: Image(image: AssetImage(items['image']??'assets/images/placeholder.png')),
+                  title: Text(items['name']),
+                  subtitle: Text("₹${items['price']}"),
+                  trailing: IconButton(
+                    onPressed: () {
+                      box.deleteAt(index);
+                    },
+                    icon: Icon(Icons.delete),
+                  ),
+                );
+              },
+            );
+          },
+        ),
+      ),
+    );
   }
 }
--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
 import 'package:mgcollection_app/screens/bottomnavigationbarScreen.dart';
-import 'package:mgcollection_app/screens/home_screen.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class LoginScreen extends StatefulWidget {
